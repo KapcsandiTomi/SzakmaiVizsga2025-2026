417637e5531542fcc5e2e8b5623f47888e9b29c2
KapcsandiTomi <102152698+KapcsandiTomi@users.noreply.github.com>
2026-04-08 15:30:22 +0200

    Update writeUs.php

--- pages/writeUs.php
+++ pages/writeUs.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../handler/maintenance_check.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aqua Mini Shop - Write Us</title>
     <link rel="stylesheet" href="../assets/css/writeUs.css">
-    <link rel="stylesheet" href="../assets/css/user-navbar.css">
+    <link rel="stylesheet" href="../assets/css/user-navbar.css?v=2">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
